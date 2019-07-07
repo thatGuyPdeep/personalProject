ce7e346005cc2f0636216d497a9f908d59b5d47f
Prabhakar Deep <thatguypdeep@gmail.com>
2019-07-07 18:30:50 +0530

    FPSGame
    Multiplayer Shooting Game

--- MultiplayerFPS/Assets/Scripts/PlayerSetup.cs
+++ MultiplayerFPS/Assets/Scripts/PlayerSetup.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
+[RequireComponent(typeof(Player))]
 public class PlayerSetup : NetworkBehaviour
 {
     [SerializeField]
@@ -28,13 +29,18 @@ public class PlayerSetup : NetworkBehaviour
                 sceneCamera.gameObject.SetActive(false);
             }
         }
-        RegisterPlayer();
+
+        GetComponent<Player>().Setup();
     }
 
-    void RegisterPlayer()
+    public override void OnStartClient()
     {
-        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
-        transform.name = _ID;
+        base.OnStartClient();
+
+        string _netID = GetComponent<NetworkIdentity>().netId.ToString();
+        Player _player = GetComponent<Player>();
+
+        GameManager.RegisterPlayer(_netID, _player);
     }
 
     void AssignRemoteLayer()
@@ -50,11 +56,16 @@ public class PlayerSetup : NetworkBehaviour
         }
     }
 
+   
+
     void OnDisable()
     {
         if(sceneCamera != null)
         {
             sceneCamera.gameObject.SetActive(true);
         }
+
+        GameManager.UnRegisterPlayer(transform.name);
+
     }
 }
